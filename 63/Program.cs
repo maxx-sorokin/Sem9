4a3eb380b228d88a2ad65ba023c7a8e6a9af7d0e
Maxim Sorokin <maximka773@mail.ru>
2022-11-26 10:38:37 +0300

    first commit

---
+++ 63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Введите N: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+PrintNumbers(n);
+
+void PrintNumbers(int n, int i = 1)
+{
+    if (i > n)
+    {
+        return;
+    }
+    Console.Write($"{i} ");
+    PrintNumbers(n, i + 1);
+}
